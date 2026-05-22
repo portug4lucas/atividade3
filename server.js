68aad55344e8a8bdff053c8a50c06734aa0d7b2a
portug4lucas <portugallucas43@gmail.com>
2026-05-22 19:29:53 -0300

    remoção do ssl

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const usersRouter = require('./routes/users');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
